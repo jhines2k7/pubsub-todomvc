a547fcf3189ebedc2ba0486b4f7d08569e488569
James Hines <jameshines10@gmail.com>
2017-04-16 13:41:30 -0400

    Creates a static version of the header component

--- js/components/Header.js
+++ js/components/Header.js
@@ -1,3 +1,28 @@
-/**
- * Created by James on 4/16/2017.
- */
+let snabbdom = require('snabbdom');
+
+let patch = snabbdom.init([ // Init patch function with chosen modules
+    require('snabbdom/modules/class').default, // makes it easy to toggle classes
+    require('snabbdom/modules/props').default, // for setting properties on DOM elements
+    require('snabbdom/modules/style').default, // handles styling on elements with support for animations
+]);
+
+let h = require('snabbdom/h').default; // helper function for creating vnodes
+
+function view() {
+    "use strict";
+
+    return h('div', [
+        h('h1', 'todos'),
+        h('input', {class: 'new-todo', props: {placeholder: 'What needs to be done?'}})
+    ]);
+}
+
+export default class HeaderComponent {
+    constructor(container) {
+        this.container = container;
+    }
+
+    render() {
+        return patch(this.container, view());
+    }
+}
