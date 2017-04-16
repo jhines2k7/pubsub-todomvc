3c4a3a17e065ce9af61d57a0c1e81f7bd3eef110
James Hines <jameshines10@gmail.com>
2017-04-16 16:44:49 -0400

    Adds an event listener to the header component that passes the value in the header text input to the keyup event handler

--- js/components/Header.js
+++ js/components/Header.js
@@ -4,18 +4,30 @@ let patch = snabbdom.init([ // Init patch function with chosen modules
     require('snabbdom/modules/class').default, // makes it easy to toggle classes
     require('snabbdom/modules/props').default, // for setting properties on DOM elements
     require('snabbdom/modules/style').default, // handles styling on elements with support for animations
-    require('snabbdom/modules/attributes').default
+    require('snabbdom/modules/attributes').default,
+    require('snabbdom/modules/eventlisteners').default
 ]);
 
 let h = require('snabbdom/h').default; // helper function for creating vnodes
 
-function view() {
+function view(component) {
     "use strict";
 
     return h('header.header', [
         h('h1', 'todos'),
-        h('input.new-todo', {props: {placeholder: 'What needs to be done?'}, attrs: {autofocus: true}})
+        h('input.new-todo', {props: {placeholder: 'What needs to be done?'}, attrs: {autofocus: true}, on: {keyup: keyUpHandler.bind(null, component)}})
     ]);
+}
+
+function keyUpHandler(component, event) {
+    "use strict";
+
+    //e.which = e.which || e.keyCode;
+    if(event.keyCode == 13) {
+        console.log(event.currentTarget.value);
+
+        event.currentTarget.value = '';
+    }
 }
 
 export default class HeaderComponent {
@@ -24,6 +36,6 @@ export default class HeaderComponent {
     }
 
     render() {
-        return patch(this.container, view());
+        return patch(this.container, view(this));
     }
 }
