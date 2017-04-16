50e2addd15c0f93f2a9164cbe7bc1b44af8500a9
James Hines <jameshines10@gmail.com>
2017-04-16 14:29:05 -0400

    Create a static version of the todo container component

--- js/components/TodoContainer.js
+++ js/components/TodoContainer.js
@@ -1,3 +1,29 @@
-/**
- * Created by James on 4/16/2017.
- */
+let snabbdom = require('snabbdom');
+
+let patch = snabbdom.init([ // Init patch function with chosen modules
+    require('snabbdom/modules/class').default, // makes it easy to toggle classes
+    require('snabbdom/modules/props').default, // for setting properties on DOM elements
+    require('snabbdom/modules/style').default, // handles styling on elements with support for animations
+    require('snabbdom/modules/attributes').default
+]);
+
+let h = require('snabbdom/h').default; // helper function for creating vnodes
+
+function view() {
+    "use strict";
+
+    return h('section.main', [
+        h('input.toggle-all', {attrs: {type: 'checkbox'}}),
+        h('label', {attrs: {for: 'toggle-all'}}, 'Mark all as complete')
+    ]);
+}
+
+export default class TodoContainerComponent {
+    constructor(container) {
+        this.container = container;
+    }
+
+    render() {
+        return patch(this.container, view());
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-import Header from './components/Header'
+import HeaderComponent from './components/Header'
+import TodoContainerComponent from './components/TodoContainer'
 
-let conainer = document.getElementById('header');
-let headerComponent = new Header(conainer);
+let headerComponent = new HeaderComponent(document.getElementById('header'));
 headerComponent.render();
+
+let todoContainerComponent = new TodoContainerComponent(document.getElementById('main'));
+todoContainerComponent.render();
