110151a407a602bef989ff60b52a2d6577310e1c
James Hines <jameshines10@gmail.com>
2017-04-16 16:11:45 -0400

    Create a static version of the todo app section footer

---
+++ js/components/TodoContainerFooter.js
@@ -0,0 +1,37 @@
+let snabbdom = require('snabbdom');
+
+let patch = snabbdom.init([ // Init patch function with chosen modules
+    require('snabbdom/modules/class').default, // makes it easy to toggle classes
+    require('snabbdom/modules/props').default, // for setting properties on DOM elements
+    require('snabbdom/modules/style').default, // handles styling on elements with support for animations
+    require('snabbdom/modules/attributes').default
+]);
+
+let h = require('snabbdom/h').default; // helper function for creating vnodes
+
+function view() {
+    "use strict";
+
+    return h('footer.footer', [
+        h('span.todo-count', [
+            h('strong', '0 items left')
+        ]),
+        h('ul.filters', [
+            h('li', [h('a.selected', {props: {href: '#/'}}, 'All')]),
+            h('li', [h('a', {props: {href: '#/active'}}, 'Active')]),
+            h('li', [h('a', {props: {href: '#/completed'}}, 'Completed')])
+        ]),
+        // Hidden if no completed items are left ↓ -->
+        h('button.clear-completed', 'Clear completed')
+    ]);
+}
+
+export default class TodoContainerFooter {
+    constructor(container) {
+     this.container = container;
+     }
+
+    render() {
+        return patch(this.container, view());
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 import HeaderComponent from './components/Header'
 import TodoContainerComponent from './components/TodoContainer'
+import TodoContainerFooter from './components/TodoContainerFooter'
 
 let headerComponent = new HeaderComponent(document.getElementById('header'));
 headerComponent.render();
 
 let todoContainerComponent = new TodoContainerComponent(document.getElementById('main'));
 todoContainerComponent.render();
+
+let todoContainerFooter = new TodoContainerFooter(document.getElementById('footer'));
+todoContainerFooter.render();
