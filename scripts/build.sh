763200836481700df7ce88ed59f25ee8750cf7de
James Hines <jhines2k7@yahoo.com>
2017-04-20 20:57:22 +0000

    Updates the buils script to copy css assets to build directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,6 +14,10 @@ node_modules/.bin/webpack --config=webpack.config.js
 # copy index.html to build directory
 cp index.html build
 
+# copy css files to build directory
+cp node_modules/todomvc-app-css/index.css build
+cp node_modules/todomvc-common/base.css build
+
 # change the src property of the script tag to app.js
 sed -i 's/build-dev\/app.js/app.js/g' build/index.html
 
