59e57ddc2071c47ce0aa33d0282a10f8be982c6f
James Hines <jhines2k7@yahoo.com>
2017-04-20 20:14:37 +0000

    Updates the build script tp qork with this project

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,15 +14,9 @@ node_modules/.bin/webpack --config=webpack.config.js
 # copy index.html to build directory
 cp index.html build
 
-# copy story.json to build directory
-cp story.json build
-
 # change the src property of the script tag to app.js
 sed -i 's/build-dev\/app.js/app.js/g' build/index.html
 
-# change the url to 'story.json'
-sed -i "s/url: '..\/story.json'/url: 'story.json'/g" build/app.js
-
-docker build -t jhines2017/pubsub .
+docker build -t jhines2017/pubsub-todomvc .
 
 date; echo;
